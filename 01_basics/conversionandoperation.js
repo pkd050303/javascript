682f6371379ee17596b19634136b31b50c42dabc
pkd050303 <156017982+pkd050303@users.noreply.github.com>
2024-01-12 15:15:58 +0000

    conversion of datatypes

---
+++ 01_basics/conversionandoperation.js
@@ -0,0 +1,25 @@
+/*let score= true
+console.log(typeof score);
+console.log(typeof (score));
+let score2 ="33"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber=Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+//"33"=>33
+//"33abc"=>NaN
+// true=>1,false=>0
+
+let isLoggedIn= "Priyam"
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+//1=>true, 0=> false
+//""=>false
+//"Priyam"=>true
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
