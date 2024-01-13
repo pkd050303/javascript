407104677807660669b35def2444c857822b615a
pkd050303 <156017982+pkd050303@users.noreply.github.com>
2024-01-13 17:13:43 +0000

    operations

--- 01_basics/conversionandoperation.js
+++ 01_basics/conversionandoperation.js
@@ -14,12 +14,48 @@ console.log(valueInNumber); */
 
 let isLoggedIn= "Priyam"
 let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+//console.log(booleanIsloggedIn);
 //1=>true, 0=> false
 //""=>false
 //"Priyam"=>true
 
 let someNumber=33
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+//*********************operations**********************/
+let value=3
+let negValue= -value
+//console.log(negValue);
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3); */
+let str1 = "hello"
+let str2 =" priyam"
+let str3 = str1+str2
+/*console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((3+4)*(5%3)); 
+console.log(+true);
+console.log(+"");*/
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter1 = 100
+gameCounter1++
+console.log(gameCounter1);
+let gameCounter2 = 100
+++gameCounter2
+console.log(gameCounter2);
+
+
